5e44bb0f3e6d693c982c18bd1e16610d5ed04b73
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-09-30 01:15:29 -0400

    Time: 3470 ms (5.03%), Space: 36.2 MB (98.78%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,19 +1,19 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        n = len(nums)
+        if not nums:
+            return False
         
-        minFromLeft = [nums[0]]
-        for i in range(1, n):
-            minFromLeft.append(min(minFromLeft[i-1], nums[i]))
+        afters = []
         
-        stack = [] 
+        min_i = [nums[0]]
+        for i in range(1, len(nums)):
+            min_i.append(min(min_i[-1], nums[i]))
         
-        for j in range(n-1, -1, -1):
-            if nums[j] > minFromLeft[j]:
-                while stack and stack[-1] <= minFromLeft[j]:
-                    stack.pop()
-                if stack and stack[-1] < nums[j]:
+        for j in reversed(range(len(nums))):
+            if nums[j] > min_i[j]:
+                index = bisect.bisect_left(afters, nums[j])
+                if index > 0 and afters[index - 1] > min_i[j]:
                     return True
-                stack.append(nums[j])
+                bisect.insort_left(afters, nums[j])
         
         return False

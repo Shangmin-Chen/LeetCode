b4d2d9cc0e21162ed572447fbcd8e574101e8ef9
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-10-03 10:26:00 -0400

    Time: 36 ms (82.94%), Space: 16.1 MB (82.24%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,16 +1,15 @@
+
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        from collections import defaultdict
-        from math import comb
-        d = defaultdict(int)
-        t = 0
-        for i in nums:
-            d[i] += 1
-        for i in d:
-            if d[i] > 2:
-                t += comb(d[i],2)
-            elif d[i] == 2:
-                t += 1
-        return t
-        
-        
+
+        diktator = {}
+        ans = 0
+
+        for i, num in enumerate(nums):
+            if num not in diktator:
+                diktator[num] = 0
+            else:
+                diktator[num] += 1
+                ans += diktator[num]
+
+        return ans

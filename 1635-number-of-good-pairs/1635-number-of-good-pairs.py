24b3e28a94d46b1c7f5e6a21ee3f90e3a310a1db
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-10-02 22:22:04 -0400

    Time: 51 ms (5.92%), Space: 16.1 MB (97.25%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,19 +1,13 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        # Create an empty dictionary to store the count of each number
-        hashMap = {}
-        # Initialize a count to keep track of the number of identical pairs
-        count = 0
-        # Iterate through each number in the input list 'nums'
-        for number in nums:
-            # If the current number is already present in the dictionary
-            if number in hashMap:
-                # Increase the 'count' by the value associated with the current number in the dictionary
-                count = count + hashMap[number]
-                # Increment the count associated with the current number in the dictionary
-                hashMap[number] += 1
-            else:
-                # If the current number is not present in the dictionary, add it with a count of 2
-                hashMap[number] = 1
-        # Return the final count of identical pairs
-        return count
+        a,b,o=0,0,0
+        while a<len(nums)-1:
+            b+=1
+            if nums[a] == nums[b]:
+                o+=1
+            if b == len(nums)-1:
+                a+=1
+                b=a
+            
+        return o
+                

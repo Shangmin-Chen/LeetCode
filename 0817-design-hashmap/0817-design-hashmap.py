3b13b341d46a4712bd06a3bbe90c6de898ed89cc
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-10-04 00:39:54 -0400

    Time: 290 ms (32.43%), Space: 41.8 MB (13.25%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,35 +1,10 @@
 class MyHashMap:
-
     def __init__(self):
-        self.size = 2333
-        self.buckets = [[] for _ in range(self.size)]
-
-    def put(self, key: int, value: int) -> None:
-        hash_val = self._hash(key)
-        if self.buckets[hash_val]:
-            for i in range(len(self.buckets[hash_val])):
-                if self.buckets[hash_val][i][0] == key:
-                    self.buckets[hash_val][i] = (key, value)
-                    return
-        self.buckets[hash_val].append((key, value))
-        
-
+        self.data = [None] * 1000001
+    def put(self, key: int, val: int) -> None:
+        self.data[key] = val
     def get(self, key: int) -> int:
-        hash_val = self._hash(key)
-        if self.buckets[hash_val]:
-            for i in range(len(self.buckets[hash_val])):
-                if self.buckets[hash_val][i][0] == key:
-                    return self.buckets[hash_val][i][1]
-        return -1
-
+        val = self.data[key]
+        return val if val != None else -1
     def remove(self, key: int) -> None:
-        hash_val = self._hash(key)
-        if self.buckets[hash_val]:
-            for i in range(len(self.buckets[hash_val])):
-                if self.buckets[hash_val][i][0] == key:
-                    del self.buckets[hash_val][i]
-                    break
-                
-
-    def _hash(self, key):
-        return hash(key) % self.size
+        self.data[key] = None

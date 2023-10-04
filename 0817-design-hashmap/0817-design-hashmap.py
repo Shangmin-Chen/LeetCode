e877de5d27c0d89397e55fade40a8f9015efbffc
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-10-04 00:34:06 -0400

    Time: 284 ms (34.29%), Space: 41.9 MB (13.25%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,37 +1,10 @@
 class MyHashMap:
-
     def __init__(self):
-        self.keys = []
-        self.values = []
-
-    def put(self, key: int, value: int) -> None:
-        try:
-            i = self.keys.index(key)
-            self.values[i] = value
-        except:
-            self.keys.append(key)
-            self.values.append(value)        
-
-
+        self.data = [None] * 1000001
+    def put(self, key: int, val: int) -> None:
+        self.data[key] = val
     def get(self, key: int) -> int:
-        try:
-            i = self.keys.index(key)
-            return self.values[i]
-        except:        
-            return -1
-
+        val = self.data[key]
+        return val if val != None else -1
     def remove(self, key: int) -> None:
-        try:
-            i = self.keys.index(key)
-            self.keys.remove(key)
-        except:
-            return -1
-        self.values.pop(i)        
-        
-
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+        self.data[key] = None

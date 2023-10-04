96de8df156245bb59cc589b59ef00b4386a64fdd
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-10-04 00:33:37 -0400

    Time: 641 ms (15.42%), Space: 19.5 MB (68.41%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,67 +1,32 @@
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
 class MyHashMap:
 
     def __init__(self):
-        self.size = 1000
-        self.hash_table = [None] * self.size 
+        self.keys = []
+        self.values = []
 
     def put(self, key: int, value: int) -> None:
-        index = key % self.size 
-    
-        if self.hash_table[index] == None:
-            # We do not have anything in this bin, just create a new node 
-            self.hash_table[index] = ListNode(key, value)
-        else:
-            # We do have something in this bin, traverse it checking to see if we have a matching key. If not just append a node to the end of the bin
-            curr_node = self.hash_table[index]
-            while True:
-                if curr_node.key == key:
-                    curr_node.value = value
-                    return 
-                if curr_node.next == None: break
-                curr_node = curr_node.next 
-            # Did not find a matching key here, so append a key, value pair in this bin
-            curr_node.next = ListNode(key, value)
+        try:
+            i = self.keys.index(key)
+            self.values[i] = value
+        except:
+            self.keys.append(key)
+            self.values.append(value)        
+
 
     def get(self, key: int) -> int:
-        index = key % self.size 
-        
-        curr_node = self.hash_table[index]
-        
-        while curr_node:
-            if curr_node.key == key:
-                return curr_node.value 
-            else:
-                curr_node = curr_node.next
-        
-        return -1
-        
+        try:
+            i = self.keys.index(key)
+            return self.values[i]
+        except:        
+            return -1
 
     def remove(self, key: int) -> None:
-        index = key % self.size 
-        
-        curr_node = prev_node = self.hash_table[index]
-        
-         # Removing from empty bin just return
-        if not curr_node: return 
-        
-        if curr_node.key == key:
-            # We found the node to delete immediately, we can now skip over it 
-            self.hash_table[index] = curr_node.next
-        else:
-            # We did not find the node to delete we must now traverse the bin
-            curr_node = curr_node.next
-            
-            while curr_node:
-                if curr_node.key == key:
-                    prev_node.next = curr_node.next 
-                    break
-                else:
-                    prev_node, curr_node = prev_node.next, curr_node.next
+        try:
+            i = self.keys.index(key)
+            self.keys.remove(key)
+        except:
+            return -1
+        self.values.pop(i)        
         
 
 

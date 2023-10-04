6b73e1e4e7dbf015d0aa27192b19675dfebf1074
Shangmin Chen <75769334+Shangmin-Chen@users.noreply.github.com>
2023-10-04 00:31:45 -0400

    Time: 222 ms (52.01%), Space: 20.1 MB (48.8%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,22 +1,38 @@
+class ListNode:
+    def __init__(self, key=-1, val=-1,next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
 class MyHashMap:
+
     def __init__(self):
-        self.d = {}
+        self.map = [ListNode() for i in range(1000)]
+
+    def hash(self, key):
+        return key % len(self.map)
+
     def put(self, key: int, value: int) -> None:
-        self.d[key] = value
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        if key in self.d:
-            return self.d[key]
-        else:
-            return -1
-
+        cur = self.map[self.hash(key)].next
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+        return -1
+        
     def remove(self, key: int) -> None:
-        if key in self.d:
-            del self.d[key]
-
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+        cur = self.map[self.hash(key)]
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
